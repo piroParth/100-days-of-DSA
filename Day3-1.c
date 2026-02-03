ff34757255f9db4fe049c3ea109132207b66f4f5
Parth Pandey <parthpandeyindian@gmail.com>
2026-02-03 20:44:51 +0530

    Add files via upload

---
+++ Day3-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int n, k;
+    int foundIndex = -1;
+    int comparisons = 0;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+    for (int i = 0; i < n; i++) {
+        comparisons++;
+        if (arr[i] == k) {
+            foundIndex = i;
+            break;
+        }
+    }
+    if (foundIndex != -1) {
+        printf("Found at index %d\n", foundIndex);
+    } else {
+        printf("Not Found\n");
+    }
+    printf("Comparisons = %d", comparisons);
+    return 0;
+}
