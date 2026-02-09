7e67be1384fe90a8a74bbaa6a81a5f63c8aa7321
Parth Pandey <parthpandeyindian@gmail.com>
2026-02-09 19:47:17 +0530

    Add files via upload

---
+++ Day9-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0;
+
+    scanf("%s", str);
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    for (i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
