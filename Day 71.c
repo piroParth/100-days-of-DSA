9c6176fbabcdf97b9ba71f264055fc72f54d97da
Parth Pandey <parthpandeyindian@gmail.com>
2026-04-12 11:41:24 +0530

    Add files via upload

---
+++ Day 71.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+#define EMPTY -1
+
+int main() {
+    int m, q;
+    scanf("%d", &m);   // size of hash table
+    scanf("%d", &q);   // number of operations
+
+    int table[m];
+
+    // Initialize table
+    for (int i = 0; i < m; i++)
+        table[i] = EMPTY;
+
+    while (q--) {
+        char op[10];
+        int key;
+        scanf("%s %d", op, &key);
+
+        int h = key % m;
+
+        if (strcmp(op, "INSERT") == 0) {
+            for (int i = 0; i < m; i++) {
+                int idx = (h + i * i) % m;
+
+                if (table[idx] == EMPTY) {
+                    table[idx] = key;
+                    break;
+                }
+            }
+        }
+
+        else if (strcmp(op, "SEARCH") == 0) {
+            int found = 0;
+
+            for (int i = 0; i < m; i++) {
+                int idx = (h + i * i) % m;
+
+                if (table[idx] == key) {
+                    found = 1;
+                    break;
+                }
+
+                if (table[idx] == EMPTY) {
+                    break; // stop early
+                }
+            }
+
+            if (found)
+                printf("FOUND\n");
+            else
+                printf("NOT FOUND\n");
+        }
+    }
+
+    return 0;
+}
